8811f4c94a982bd84cf120f532ce6bf53bedfd88
artemmer <art.meriuts@gmail.com>
2022-04-29 15:23:21 +0200

    r

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -11,7 +11,6 @@ public class Phone {
   this.memory = memory;
   this.displaySize = displaySize;
  }
- 
  @Override
  public String toString() {
   return "Phone{" +
@@ -22,9 +21,17 @@ public class Phone {
           '}';
  }
 
+ public static void main(String[] args) {
+  Phone phone1 = new Phone("plastic", "ips", 128, 3);
+  Phone phone2 = new Phone("metal", "lcd", 512, 6);
+  Phone phone3 = new Phone("glass", "oled", 16, 5);
 
 
+  System.out.println(phone1.toString());
+  System.out.println(phone2.toString());
+  System.out.println(phone3.toString());
 
+
+ }
 
 }
-
